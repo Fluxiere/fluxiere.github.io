eec702a19d4206a948a8866b02505edc9e4b5ba6
Vasanth kumar AM <vasanthkumar17112002@gmail.com>
2026-06-22 14:16:13 +0530

    Added FevIcon and servivce section in home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,14 @@
 import { Hero } from '../../components/Hero/Hero';
 import { TrustStrip } from '../../components/TrustStrip/TrustStrip';
 import { StackStrip } from '../../components/StackStrip/StackStrip';
+import Services from '../Services/Services';
 // import { Results } from '../../components/Results/Results';
 
 const Home = () => {
   return (
     <main style={{ marginTop: '68px' }}>
       <Hero />
+      <Services />
       <TrustStrip />
       <StackStrip />
       {/* <Results /> */}
