7069905e86553305f6e89596fb01fe5fb076159c
Vasanth kumar AM <vasanthkumar17112002@gmail.com>
2026-06-23 12:00:49 +0530

    changed Base-name in viteConfig

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/fluxiere/' : '/', // Must match your exact GitHub repository name wrapped in slashes
+  // base: process.env.NODE_ENV === 'production' ? '/fluxiere/' : '/', // Must match your exact GitHub repository name wrapped in slashes
+  base: '/',
 })
